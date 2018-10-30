27514219918610a6a727148e788d8346b9c43419
unknown <ervin@aben.ch>
2018-10-30 14:39:50 +0100

    Total expense calculation added

---
+++ src/tests/components/ExpenseSummary.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { ExpensesSummary } from '../../components/ExpenseSummary';
+
+test('should correctly render ExpensesSummarry with 1 expense', () => {
+const wrapper = shallow(<ExpensesSummary expenseCount={1} expensesTotal={235}/>);
+expect(wrapper).toMatchSnapshot();
+
+});
+
+
+test('should correctly render ExpensesSummarry with multiples expenses',
+    () => {
+        const wrapper = shallow(<ExpensesSummary expenseCount={23} expensesTotal={23512340987}/>);
+        expect(wrapper).toMatchSnapshot();
+
+    });
